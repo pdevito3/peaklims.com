80df20b8269d12c1e41ebd8c6ca7cf01cb913997
Paul DeVito <pdevito3@gmail.com>
2024-12-06 22:30:16 -0500

    fix: mobile

--- app/routes/email-subscription-confirmed.tsx
+++ app/routes/email-subscription-confirmed.tsx
@@ -11,7 +11,7 @@ function RouteComponent() {
   return (
     <div className="w-full h-svh flex flex-col items-center justify-center">
       <img src={Logo} alt="Peak LIMS" className="h-16" />
-      <div className="mt-6 border-2 border-emerald-600 rounded-lg p-10 max-w-2xl bg-emerald-300/50">
+      <div className="mt-6 md:border-2 md:border-emerald-600 rounded-lg p-10 max-w-2xl md:bg-emerald-300/50">
         <h1 className="text-3xl font-bold">Subscription Confirmed!</h1>
         <p className="text-lg mt-4">
           Boom! You're officially confirmed and on the list. Expect some great
