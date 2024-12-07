437727b0994e48cb86336a9236fde56f22d6c3a2
Paul DeVito <pdevito3@gmail.com>
2024-12-06 21:01:25 -0500

    fix: import alias for mdx

--- app.config.ts
+++ app.config.ts
@@ -1,17 +1,15 @@
 import mdx from "@mdx-js/rollup";
 import { defineConfig } from "@tanstack/start/config";
-// import path from "path";
+import path from "path";
 import rehypeAddClasses from "rehype-add-classes";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import rehypeSlug from "rehype-slug";
-// import { fileURLToPath } from "url";
-import tsConfigPaths from "vite-tsconfig-paths";
-// import collectHeadings from "~/plugins/collect-headings";
-
 import { visit } from "unist-util-visit";
+import { fileURLToPath } from "url";
+import tsConfigPaths from "vite-tsconfig-paths";
 
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   routers: {
@@ -26,11 +24,11 @@ export default defineConfig({
     preset: "vercel",
   },
   vite: {
-    // resolve: {
-    //   alias: {
-    //     "~": path.resolve(__dirname, "./app"),
-    //   },
-    // },
+    resolve: {
+      alias: {
+        "~": path.resolve(__dirname, "./app"),
+      },
+    },
     plugins: [
       mdx({
         remarkPlugins: [collectHeadings],
